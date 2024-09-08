1d5006f93542081aab9ae23d0738ff0c1959a687
Vassa42 <riccardo.vassanelli.rv@gmail.com>
2024-09-08 15:37:37 +0200

    changes

--- psn_evaluator.py
+++ psn_evaluator.py
@@ -1,9 +1,10 @@
 import customtkinter as ctk
+from tkinter import filedialog
 
 class PSNEvaluator:
     def __init__(self, root):
         self.root = root
-        self.root.title("PSN evaluator")
+        self.root.title("PSN Evaluator")
 
         # Imposta il tema di customtkinter
         ctk.set_appearance_mode("dark")
@@ -17,62 +18,87 @@ class PSNEvaluator:
         self.frame = ctk.CTkFrame(self.outer_frame, fg_color='lightgreen')
         self.frame.pack(expand=True, padx=10, pady=10)
 
-        # Selezione della soglia
-        self.label_threshold = ctk.CTkLabel(self.frame, text="Select the threshold:", font=("Helvetica", 50), text_color="black")
-        self.label_threshold.grid(row=0, column=0, padx=10, pady=10)
+        # Selezione del file macro_IIN
+        self.label_macro_IIN_file = ctk.CTkLabel(self.frame, text="Select the macro IIN file:", font=("Helvetica", 40), text_color="black")
+        self.label_macro_IIN_file.grid(row=0, column=0, padx=10, pady=10)
 
-        self.threshold_var = ["0.5", "1.0", "1.5", "2.0"]
-        self.selected_threshold_var = ctk.StringVar()
-        self.combobox_threshold = ctk.CTkComboBox(self.frame, variable=self.selected_threshold_var, values=self.threshold_var, width=150, height=40, font=("Helvetica", 35))
-        self.combobox_threshold.grid(row=0, column=1, padx=10, pady=10)
-        self.combobox_threshold.set(self.threshold_var[1])  # Valore di default
+        self.button_browse_macro_IIN = ctk.CTkButton(self.frame, text="Browse", font=("Helvetica", 35), command=self.browse_macro_IIN_file, width=150, fg_color="gray30")
+        self.button_browse_macro_IIN.grid(row=1, column=1, padx=10, pady=10)
 
-        # Selezione delle metriche aggiuntive
-        self.label_metrics = ctk.CTkLabel(self.frame, text="Select the additional metrics:", font=("Helvetica", 50), text_color="black")
-        self.label_metrics.grid(row=1, column=0, padx=10, pady=10)
+        self.macro_IIN_file_var = ctk.StringVar(value="")
+        self.entry_macro_IIN_file = ctk.CTkEntry(self.frame, textvariable=self.macro_IIN_file_var, font=("Helvetica", 25), width=250)
+        self.entry_macro_IIN_file.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
-        self.metrics_var = ["Metric1", "Metric2", "Metric3"]
-        self.selected_metrics_var = ctk.StringVar()
-        self.combobox_metrics = ctk.CTkComboBox(self.frame, variable=self.selected_metrics_var, values=self.metrics_var, width=150, height=40, font=("Helvetica", 35))
-        self.combobox_metrics.grid(row=1, column=1, padx=10, pady=10)
-        self.combobox_metrics.set(self.metrics_var[0])  # Valore di default
+        # Selezione del file di struttura
+        self.label_structure_file = ctk.CTkLabel(self.frame, text="Select the structure file:", font=("Helvetica", 40), text_color="black")
+        self.label_structure_file.grid(row=2, column=0, padx=10, pady=10)
 
-        # Selezione dei formati di output
-        self.label_output = ctk.CTkLabel(self.frame, text="Select the output formats:", font=("Helvetica", 50), text_color="black")
-        self.label_output.grid(row=2, column=0, padx=10, pady=10)
+        self.button_browse_structure = ctk.CTkButton(self.frame, text="Browse", font=("Helvetica", 35), command=self.browse_structure_file, width=150, fg_color="gray30")
+        self.button_browse_structure.grid(row=3, column=1, padx=10, pady=10)
 
-        self.var_gml = ctk.BooleanVar()
-        self.var_png = ctk.BooleanVar()
-        self.var_pml = ctk.BooleanVar()
-        self.var_vmd = ctk.BooleanVar()
-        self.var_pdb = ctk.BooleanVar()
+        self.structure_file_var = ctk.StringVar(value="")
+        self.entry_structure_file = ctk.CTkEntry(self.frame, textvariable=self.structure_file_var, font=("Helvetica", 25), width=250)
+        self.entry_structure_file.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
-        self.check_gml = ctk.CTkCheckBox(self.frame, text=".gml", variable=self.var_gml, font=("Helvetica", 35), text_color="black")
-        self.check_png = ctk.CTkCheckBox(self.frame, text=".png", variable=self.var_png, font=("Helvetica", 35), text_color="black")
-        self.check_pml = ctk.CTkCheckBox(self.frame, text="PyMOL script (.pml)", variable=self.var_pml, font=("Helvetica", 35), text_color="black")
-        self.check_vmd = ctk.CTkCheckBox(self.frame, text="VMD script (.vmd)", variable=self.var_vmd, font=("Helvetica", 35), text_color="black")
-        self.check_pdb = ctk.CTkCheckBox(self.frame, text=".pdb", variable=self.var_pdb, font=("Helvetica", 35), text_color="black")
+        # Selezione delle metriche da valutare
+        self.label_metrics = ctk.CTkLabel(self.frame, text="Select the metrics to evaluate:", font=("Helvetica", 40), text_color="black")
+        self.label_metrics.grid(row=4, column=0, padx=10, pady=10)
 
-        self.check_gml.grid(row=3, column=0, sticky='w', padx=10)
-        self.check_png.grid(row=3, column=1, sticky='w', padx=10)
-        self.check_pml.grid(row=4, column=0, sticky='w', padx=10)
-        self.check_vmd.grid(row=4, column=1, sticky='w', padx=10)
-        self.check_pdb.grid(row=5, column=0, sticky='w', padx=10)
+        self.var_graph_analysis = ctk.BooleanVar()
+        self.var_path_analysis = ctk.BooleanVar()
+        self.var_centrality_analysis = ctk.BooleanVar()
+
+        self.check_graph_analysis = ctk.CTkCheckBox(self.frame, text="Graph Analysis", variable=self.var_graph_analysis, font=("Helvetica", 35), text_color="black")
+        self.check_path_analysis = ctk.CTkCheckBox(self.frame, text="Path Analysis", variable=self.var_path_analysis, font=("Helvetica", 35), text_color="black")
+        self.check_centrality_analysis = ctk.CTkCheckBox(self.frame, text="Centrality Analysis", variable=self.var_centrality_analysis, font=("Helvetica", 35), text_color="black")
+
+        self.check_graph_analysis.grid(row=5, column=0, sticky='w', padx=10)
+        self.check_path_analysis.grid(row=5, column=1, sticky='w', padx=10)
+        self.check_centrality_analysis.grid(row=6, column=0, sticky='w', padx=10)
 
         # Bottone per avviare il processo
         self.button_process = ctk.CTkButton(self.frame, text="Process", font=("Helvetica", 35), command=self.process_files, width=150, height=40, fg_color="gray30")
-        self.button_process.grid(row=6, column=0, columnspan=2, pady=20)
+        self.button_process.grid(row=7, column=0, columnspan=2, pady=20)
 
         # Etichetta per il risultato
         self.label_result = ctk.CTkLabel(self.frame, text="", font=("Helvetica", 35), text_color="black")
-        self.label_result.grid(row=7, column=0, columnspan=2, pady=10)
+        self.label_result.grid(row=8, column=0, columnspan=2, pady=10)
+
+    def browse_macro_IIN_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Dat files", "*.dat")])
+        if file_path:
+            self.macro_IIN_file_var.set(file_path)
+
+    def browse_structure_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Pdb files", "*.pdb"), ("Gro files", "*.gro")])
+        if file_path:
+            self.structure_file_var.set(file_path)
 
     def process_files(self):
-        threshold = self.selected_threshold_var.get()
-        metrics = self.selected_metrics_var.get()
-        if not threshold or not metrics:
-            self.label_result.config(text="Error: Select a threshold and the additional metrics.", text_color='red')
+        macro_IIN_file = self.macro_IIN_file_var.get()
+        structure_file = self.structure_file_var.get()
+        graph_analysis = self.var_graph_analysis.get()
+        path_analysis = self.var_path_analysis.get()
+        centrality_analysis = self.var_centrality_analysis.get()
+
+        if not macro_IIN_file or not structure_file:
+            self.label_result.config(text="Error: Select both macro IIN file and structure file.", text_color='red')
             return
+
+        selected_metrics = []
+        if graph_analysis:
+            selected_metrics.append("Graph Analysis")
+        if path_analysis:
+            selected_metrics.append("Path Analysis")
+        if centrality_analysis:
+            selected_metrics.append("Centrality Analysis")
+
+        if not selected_metrics:
+            self.label_result.config(text="Error: Select at least one metric.", text_color='red')
+            return
+
+        # Messaggio di conferma, per ora non implementa la logica del processo
+        self.label_result.config(text=f"Processing with macro IIN file: {macro_IIN_file}\nFile: {structure_file}\nMetrics: {', '.join(selected_metrics)}", text_color='green')
 
 if __name__ == "__main__":
     root = ctk.CTk()
