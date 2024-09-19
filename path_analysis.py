a777a6dfc45a64cfcf3eddc53d3cc188b251d9eb
Vassa42 <riccardo.vassanelli.rv@gmail.com>
2024-09-19 14:45:27 +0200

    changes

--- path_analysis.py
+++ path_analysis.py
@@ -17,6 +17,40 @@ class PathAnalysis:
         self.frame = ctk.CTkFrame(self.outer_frame, fg_color="DarkOliveGreen1")
         self.frame.pack(expand=True, padx=10, pady=10)
 
+        self.label_start_list = ctk.CTkLabel(self.frame, text="Insert the start's list of residues:", font=("Helvetica", 40),
+                                            text_color="black")
+        self.label_start_list.grid(row=0, column=0, padx=10, pady=10)
+
+        self.entry_start_list = ctk.CTkEntry(self.frame, width=200, height=40, font=("Helvetica", 40), justify="center")
+        self.entry_start_list.grid(row=0, column=1, padx=10, pady=10)
+
+        self.button_cutOff = ctk.CTkButton(self.frame, text="Confirm", font=("Helvetica", 40),
+                                           command=self.confirm_start, fg_color="gray30", width=200)
+        self.button_cutOff.grid(row=0, column=2, padx=10, pady=10)
+
+        self.label_end_list = ctk.CTkLabel(self.frame, text="Insert the end's list of residues:",
+                                             font=("Helvetica", 40),
+                                             text_color="black")
+        self.label_end_list.grid(row=1, column=0, padx=10, pady=10)
+
+        self.entry_end_list = ctk.CTkEntry(self.frame, width=200, height=40, font=("Helvetica", 40), justify="center")
+        self.entry_end_list.grid(row=1, column=1, padx=10, pady=10)
+
+        self.button_cutOff = ctk.CTkButton(self.frame, text="Confirm", font=("Helvetica", 40),
+                                           command=self.confirm_end, fg_color="gray30", width=200)
+        self.button_cutOff.grid(row=1, column=2, padx=10, pady=10)
+
+        self.process_button = ctk.CTkButton(self.frame, text="Process", font=("Helvetica", 40), width=200, height=40,
+                                            fg_color="gray30")
+        self.process_button.grid(row=2, column=1, padx=10, pady=20)
+
+    def confirm_start(self):
+        self.entry_start_list.get()
+
+    def confirm_end(self):
+        self.entry_end_list.get()
+
+
 if __name__ == "__main__":
     root = ctk.CTk()
     app = PathAnalysis(root)
